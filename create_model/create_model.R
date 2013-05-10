ddb14ea24d3942d971c210fe9364e09df3e3f6be
corynissen <corynissen@gmail.com>
2013-05-10 13:28:11 -0500

    cleaned up the code a bit

--- create_model/create_model.R
+++ create_model/create_model.R
@@ -25,36 +25,38 @@ remove.word <- function(string, starts.with.char){
   return(ret.string)
 }
 
+clean.text <- function(text){
+  # INPUT: Text to be "cleansed"
+  # OUTPUT: Cleansed text
+  # USAGE: clean.text(text) will return a string that has the punctuation removed
+  #        lower case, and all other text cleaning operations done
+  text.cleansed <- tolower(text)
+  # remove the string "food poisoning" because every tweet has this in it...
+  text.cleansed <- gsub("food poisoning", "", text.cleansed)
+  text.cleansed <- replace.links(text.cleansed)
+  text.cleansed <- remove.word(text.cleansed, "@")
+  text.cleansed <- remove.word(text.cleansed, "rt")
+  # replace non-letters with spaces
+  text.cleansed <- gsub("[^[:alnum:]]", " ", text.cleansed)
+  # remove leading and trailing spaces
+  text.cleansed <- gsub("^\\s+|\\s+$", "", text.cleansed)
+  # replace multiple spaces next to each other with single space
+  text.cleansed <- gsub("\\s{2,}", " ", text.cleansed)
+  return(text.cleansed)
+}  
+
 # read the manually classified tweets
 df <- read.csv("create_model/fp_tweets.csv", stringsAsFactors=F)
 
 # do the preprocessing to the data. this needs to be done before any prediction
 # using the model that is created.
-df$text.cleansed <- tolower(df$text)
-# remove the string "food poisoning" because every tweet has this in it...
-df$text.cleansed <- sapply(df$text.cleansed,
-                           function(x)gsub("food poisoning", "", x))
-df$text.cleansed <- as.character(sapply(df$text.cleansed, replace.links))
-df$text.cleansed <- as.character(sapply(df$text.cleansed,
-                                        function(x)remove.word(x, "@")))
-df$text.cleansed <- as.character(sapply(df$text.cleansed,
-                                        function(x)remove.word(x, "rt")))
-# replace non-letters with spaces
-df$text.cleansed <- as.character(sapply(df$text.cleansed,
-                                        function(x)gsub("[^[:alnum:]]", " ", x)))
-# remove leading and trailing spaces
-df$text.cleansed <- as.character(sapply(df$text.cleansed,
-                                        function(x)gsub("^\\s+|\\s+$", "", x)))
-# replace multiple spaces next to each other with single space
-df$text.cleansed <- as.character(sapply(df$text.cleansed,
-                                        function(x)gsub("\\s{2,}", " ", x)))
-
+df$text.cleansed <- as.character(sapply(df$text, function(x)clean.text(x)))
 df <- subset(df, text.cleansed!="")
 
 # train the model using the textcat package
 fp.model <- textcat_profile_db(df$text.cleansed, df$manual_class)
 # save model file to be used on server 
-save(list=c("fp.model", "replace.links", "remove.word"),
+save(list=c("fp.model", "replace.links", "remove.word", "clean.text"),
      file="fp_model.Rdata")
 
 # test it out...
