8074363a4f58e67354fc9915d7f7f5da6fde3ed3
corynissen <corynissen@gmail.com>
2013-05-10 13:27:34 -0500

    test new script

---
+++ fp_classifier_test.R
@@ -0,0 +1,22 @@
+
+run <- function(text){
+  load("/var/FastRWeb/web.R/rdata/fp_model.Rdata")
+
+  text.cleansed <- clean.text(text)
+  result <- textcat(text.cleansed, fp.model)
+  
+  if(is.na(result)){
+    result.print <- "don't know..."
+  }else{
+    if(result=="1"){
+      result.print <- "food poisoning tweet"
+    }else{
+      if(result=="0"){
+        result.print <- "not a food poisoning tweet"
+      }else{
+        result.print <- "something is wrong..."
+      }
+    }
+  }
+  WebResult(result.print)
+}
