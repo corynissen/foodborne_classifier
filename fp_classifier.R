436b1cf4771014946a8a76597c74a48bdd774f33
corynissen <corynissen@gmail.com>
2013-05-10 14:49:35 -0500

    updated with a little cleaner code

--- fp_classifier.R
+++ fp_classifier.R
@@ -1,20 +1,7 @@
 
 run <- function(text){
   load("/var/FastRWeb/web.R/rdata/fp_model.Rdata")
-
-  text.cleansed <- tolower(text)
-  # remove the string "food poisoning" because every tweet has this in it...
-  text.cleansed <- gsub("food poisoning", "", text.cleansed)
-  text.cleansed <- replace.links(text.cleansed)
-  text.cleansed <- remove.word(text.cleansed, "@")
-  text.cleansed <- remove.word(text.cleansed, "rt")
-  # replace non-letters with spaces
-  text.cleansed <- gsub("[^[:alnum:]]", " ", text.cleansed)
-  # remove leading and trailing spaces
-  text.cleansed <- gsub("^\\s+|\\s+$", "", text.cleansed)
-  # replace multiple spaces next to each other with single space
-  text.cleansed <- gsub("\\s{2,}", " ", text.cleansed)
-
+  text.cleansed <- clean.text(text)
   result <- textcat(text.cleansed, fp.model)
   
   if(is.na(result)){
